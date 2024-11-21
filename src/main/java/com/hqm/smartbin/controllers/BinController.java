8734a30433d3decabdb6f941ea484472da985c95
HQM <nur.hakim.arif@gmail.com>
2024-11-21 11:38:34 +0700

    input data

--- src/main/java/com/hqm/smartbin/controllers/BinController.java
+++ src/main/java/com/hqm/smartbin/controllers/BinController.java
@@ -2,13 +2,20 @@ package com.hqm.smartbin.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/bin")
 public class BinController {
 
-    @GetMapping(value = "/bin")
+    @GetMapping(value = "")
     public String index(){
 
         return "bin";
+    }
+
+    @GetMapping(value = "/new")
+    public String baru(){
+        return "bin-new" ;
     }
 }
