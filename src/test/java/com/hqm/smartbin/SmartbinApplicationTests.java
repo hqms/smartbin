6820fda360b7a28679779d5f1e679457a80f0851
HQM <nur.hakim.arif@gmail.com>
2024-11-21 09:41:39 +0700

    init project

---
+++ src/test/java/com/hqm/smartbin/SmartbinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hqm.smartbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
